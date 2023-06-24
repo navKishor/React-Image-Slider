9e2d3e2ce2f6d5e7d1f28ba83b94a96f2e336477
Novel <rsj11496@gmail.com>
2023-06-24 12:04:46 +0530

    added swiper library and ion icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,80 @@
 import React from 'react';
-import Image1 from "./Components/images/img_1.jpg";
-import Image2 from "./Components/images/img_2.jpg";
-import Image3 from "./Components/images/img_3.jpg";
-import Image4 from "./Components/images/img_4.jpg";
-import Image5 from "./Components/images/img_5.jpg";
-import Image6 from "./Components/images/img_6.jpg";
-import Image7 from "./Components/images/img_7.jpg";
-import "./App.css";
-import ImageCarousel from './Components/ImageCaraousal';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
-const App = () => {
-  const images = [Image1, Image2, Image3, Image4, Image5, Image6, Image7];
+import { EffectCoverflow, Pagination, Navigation } from 'swiper';
 
+import slide_image_1 from './Components/images/img_1.jpg';
+import slide_image_2 from './Components/images/img_2.jpg';
+import slide_image_3 from './Components/images/img_3.jpg';
+import slide_image_4 from './Components/images/img_4.jpg';
+import slide_image_5 from './Components/images/img_5.jpg';
+import slide_image_6 from './Components/images/img_6.jpg';
+import slide_image_7 from './Components/images/img_7.jpg';
+
+function App() {
   return (
-    <div className="app">
-      <div class="four">
-      <h1><span>Beautiful Slides</span> React <em>Image</em> Slider</h1>
+    <div className="container">
+      <h1 className="heading">Flower Gallery</h1>
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 90,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: '.swiper-pagination', clickable: true }}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container"
+      >
+        <SwiperSlide>
+          <img src={slide_image_1} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_2} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_3} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_4} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_5} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_6} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_7} alt="slide_image" />
+        </SwiperSlide>
+
+        <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div>
+          <div className="swiper-pagination"></div>
+        </div>
+      </Swiper>
     </div>
-      <ImageCarousel images={images} />
-      </div>
   );
-};
+}
 
 export default App;
